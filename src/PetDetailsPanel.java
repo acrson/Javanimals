3de2b7f783037f46c989eaa02f9e88b65c29f8de
ccrutcher1029 <ccrutcher@cub.uca.edu>
2024-12-02 11:17:15 -0600

    Worked on the details panel, about to make sure everything is functioning properly

--- src/PetDetailsPanel.java
+++ src/PetDetailsPanel.java
@@ -1,13 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PetDetailsPanel extends JPanel {
-    public PetDetailsPanel() {
+    private JButton toggleButton;
+    private JPanel detailsPanel;
+    private boolean isExpanded = false;
+
+    public PetDetailsPanel(Pet pet) {
         Color skyBlue = new Color(90, 150, 255);
         this.setPreferredSize(new Dimension(400, 400));
         this.setBackground(skyBlue);
 
-        JLabel titleLabel = new JLabel("PetDetailsPanel");
-        add(titleLabel);
+        toggleButton = new JButton("PetDetailsPanel");
+        toggleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isExpanded = !isExpanded;
+                detailsPanel.setVisible(isExpanded);
+                toggleButton.setText(isExpanded ? "Hide Details" : "Show Details");
+            }
+        });
+        add(toggleButton);
+
+        detailsPanel = new JPanel();
+        detailsPanel.setBackground(skyBlue);
+        detailsPanel.add(new JLabel("Name: " + pet.getName()));
+        detailsPanel.add(new JLabel("Sex: " + pet.getSex()));
+        detailsPanel.add(new JLabel("Age: " + pet.getCurrentAge()));
+        detailsPanel.add(new JLabel("Color: " + pet.getColor()));
+        detailsPanel.add(new JLabel("Life Expectancy: " + pet.getLifeExpectancy()));
+        detailsPanel.add(new JLabel("Breed: " + pet.getType()));
+        detailsPanel.setVisible(false);
     }
 }
