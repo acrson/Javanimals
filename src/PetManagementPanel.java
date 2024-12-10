e6fb039a44c589f92c5707a12dd53c4d0665a30b
ccrutcher1029 <ccrutcher@cub.uca.edu>
2024-12-10 14:11:21 -0600

    The attention meters are added but need to show where wanted

--- src/PetManagementPanel.java
+++ src/PetManagementPanel.java
@@ -2,16 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-/*
-I changed some things in this PetManagementPanel, once clicking on an item to buy, the shopPanel then asks the
-user to write in the name of the pet(before I just had the breed) once it asked that and I would type in the relevant
-name it would say the animal did not exist. After tyring to work some things out I realized I might have to
-revamp some things in this class. Instead of having separate arrays for the all the animals, I utilized it into a
-single map where the key is the pets name. I think changing into a map will be good for efficient management too.
- */
+import java.util.TimerTask;
+import java.util.Timer;
 
 public class PetManagementPanel extends JPanel {
-    private static final int MAX_PETS = 4; //Maximum number of pets in the array
+    private static final int MAX_PETS = 4; // Maximum number of pets in the array
+    private Pet[] pets; // Array to store all pets
+    private Dog[] dogs; // Array to store all dogs
+    private Cat[] cats; // Array to store all cats
+    private Bird[] birds; // Array to store all birds
+    private Fish[] fish; // Array to store all fish
+    private int petCount; // Keeps track of the number of pets in the array
+    private int dogCount; // Keeps track of the number of dogs
+    private int catCount; // Keeps track of the number of cats
+    private int birdCount; // Keeps track of the number of birds
+    private int fishCount; // Keeps track of the number of fish
     boolean uniqueName = true;
     Color Brown = new Color(100, 60, 25);
     Color Beige = new Color(207, 185, 151);
@@ -21,8 +26,20 @@ public class PetManagementPanel extends JPanel {
     String birdimg = "\n\n\n  .--.   \n <(o )___\n  (   ._> \n `----'   \n\n\n";
     String fishimg = "\n\n\n\n><(((('>\n\n\n";
 
-    //Constructor
+    // Constructor
     public PetManagementPanel(PetDisplayPanel petDisplayPanel, PetDetailsPanel petDetailsPanel) {
+        pets = new Pet[MAX_PETS];
+        dogs = new Dog[MAX_PETS];
+        cats = new Cat[MAX_PETS];
+        birds = new Bird[MAX_PETS];
+        fish = new Fish[MAX_PETS];
+        petCount = 0;
+        dogCount = 0;
+        catCount = 0;
+        birdCount = 0;
+        fishCount = 0;
+        JButton[] DetailsButtons = new JButton[MAX_PETS];
+
         this.setPreferredSize(new Dimension(400, 400));
         this.setBackground(Color.lightGray);
 
@@ -62,100 +79,178 @@ public class PetManagementPanel extends JPanel {
         this.add(formPanel, BorderLayout.CENTER);
         this.add(buttonPanel, BorderLayout.SOUTH);
 
-// Button Action Listener
-        createPetButton.addActionListener(e -> {
-            String name = nameField.getText();
-            String type = (String) typeComboBox.getSelectedItem();
-            Color color = getColorFromName((String) colorComboBox.getSelectedItem());
-            char sex = (Character) sexComboBox.getSelectedItem();
+        // Button Action Listener
+        createPetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                uniqueName = true;
+                String name = nameField.getText();
+                String type = (String) typeComboBox.getSelectedItem();
+                Color color = getColorFromName((String) colorComboBox.getSelectedItem());
+                char sex = (Character) sexComboBox.getSelectedItem();
 
-            // Check for duplicate names
-            if (AnimalGUI.petDatabase.containsKey(name)) {
-                JOptionPane.showMessageDialog(this, "Pet with name \"" + name + "\" already exists!");
-                return;
+                // Default attributes for simplicity
+                int health = 10;
+                int attention = 10;
+                int currentAge = 1;
+                int lifeExpectancy = 10;
+
+                System.out.println(petCount);
+
+                // Checks whether the created pet's name already exists in the game
+                if (petCount > 0) {
+                    for (int i = 0; i < petCount; i++) {
+                        if (name.equals(pets[i].getName())) {
+                            uniqueName = false;
+                            break;
+                        }
+                    }
+                }
+
+                Pet newPet = null;
+
+                // Create the appropriate type of pet
+                switch (type) {
+                    case "Dog":
+                        if (dogCount < MAX_PETS) {
+                            newPet = new Dog("Unknown Breed", name, "Dog", attention, health, currentAge, lifeExpectancy, color, sex);
+                            //dogs[dogCount++] = (Dog) newPet; // Add dog to array and increment count
+                        }
+                        break;
+                    case "Cat":
+                        if (catCount < MAX_PETS) {
+                            newPet = new Cat(name, "Cat", attention, health, currentAge, lifeExpectancy, color, sex, "Unknown Breed");
+                            //cats[catCount++] = (Cat) newPet; // Add cat to array and increment count
+                        }
+                        break;
+                    case "Bird":
+                        if (birdCount < MAX_PETS) {
+                            newPet = new Bird("Unknown Breed", name, "Bird", attention, health, currentAge, lifeExpectancy, color, sex);
+                            //birds[birdCount++] = (Bird) newPet; // Add bird to array and increment count
+                        }
+                        break;
+                    case "Fish":
+                        if (fishCount < MAX_PETS) {
+                            newPet = new Fish(name, "Fish", attention, health, currentAge, lifeExpectancy, color, sex, "Unknown Breed");
+                            //fish[fishCount++] = (Fish) newPet; // Add fish to array and increment count
+                        }
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Invalid pet type.");
+                        return;
+                }
+
+                // Go on with creating the new pet, only if there IS a pet, you have less than 4 pets, and the pet has a unique name.
+                if (newPet != null && petCount < MAX_PETS && uniqueName) {
+
+                    pets[petCount++] = newPet; // Add to the main pets array
+                    newPet.ID = petCount; // Assign the proper ID number to the pet (first pet gets ID of 1) to keep track
+
+                    // Create a new panel for the pet and display its name
+                    JPanel petPanel = new JPanel();
+                    petPanel.setLayout(new BoxLayout(petPanel, BoxLayout.Y_AXIS));  // Stacks components vertically
+                    petPanel.setPreferredSize(new Dimension(262, 242));
+                    petPanel.setBackground(newPet.getColor());
+
+                    JLabel petNameLabel = new JLabel(newPet.getName());
+
+                    JLabel petImageLabel = new JLabel("n/a");
+
+                    //Add the attention meter
+                    JProgressBar attentionMeter = new JProgressBar(0,10);
+                    attentionMeter.setValue(newPet.getAttention());
+                    attentionMeter.setStringPainted(true);
+
+                    Timer attentionTimer = new Timer();
+                    Pet finalNewPet = newPet;
+                    attentionTimer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            finalNewPet.decreaseAttention();
+                            attentionMeter.setValue(finalNewPet.getAttention());
+                        }
+                    }, 0, 30000);
+
+                    switch (newPet.getType()) {
+                        case "Dog":
+                            String formattedDogImg = "<html><div style='text-align: center; font-family: monospace;'>" +
+                                    dogimg.replace(" ", "&nbsp;")
+                                            .replace("<", "&lt;") // Escape the < character
+                                            .replace("\n", "<br>") +
+                                    "</div></html>";
+                            petImageLabel = new JLabel(formattedDogImg);
+                            break;
+                        case "Cat":
+                            String formattedCatImg = "<html><div style='text-align: center; font-family: monospace;'>" +
+                                    catimg.replace(" ", "&nbsp;")
+                                            .replace("<", "&lt;") // Escape the < character
+                                            .replace("\n", "<br>") +
+                                    "</div></html>";
+                            petImageLabel = new JLabel(formattedCatImg);
+                            break;
+                        case "Bird":
+                            String formattedBirdImg = "<html><div style='text-align: center; font-family: monospace;'>" +
+                                    birdimg.replace(" ", "&nbsp;")
+                                            .replace("<", "&lt;") // Escape the < character
+                                            .replace("\n", "<br>") +
+                                    "</div></html>";
+                            petImageLabel = new JLabel(formattedBirdImg);
+                            break;
+                        case "Fish":
+                            String formattedFishImg = "<html><div style='text-align: center; font-family: monospace;'>" +
+                                    fishimg.replace(" ", "&nbsp;")
+                                            .replace("<", "&lt;") // Escape the < character
+                                            .replace("\n", "<br>") +
+                                    "</div></html>";
+                            petImageLabel = new JLabel(formattedFishImg);
+                            break;
+                    }
+
+                    if (newPet.getColor() == Color.BLACK || newPet.getColor() == Color.blue
+                            || newPet.getColor() == Brown || newPet.getColor() == Color.darkGray)
+                    {
+                        petNameLabel.setForeground(Color.white);
+                        petImageLabel.setForeground(Color.white);
+                    }
+
+                    // Add a button to view pet details
+                    DetailsButtons[petCount-1] = new JButton("View Details");
+                    DetailsButtons[petCount-1].setAlignmentX(Component.CENTER_ALIGNMENT);
+
+                    int currentPetIndex = petCount - 1;
+                    DetailsButtons[currentPetIndex].addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            petDetailsPanel.showPetDetails(pets[currentPetIndex]); // Use the captured index
+                        }
+                    });
+
+
+                    petNameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+                    petImageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+                    petNameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+                    petImageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+                    petPanel.add(petNameLabel, BorderLayout.CENTER);
+                    petPanel.add(petImageLabel, BorderLayout.CENTER);
+                    petPanel.add(DetailsButtons[petCount-1], BorderLayout.SOUTH);
+
+                    // Add the new pet panel to the PetDisplayPanel
+                    petDisplayPanel.addPetPanel(petPanel);
+
+                    System.out.println(petCount);
+
+                    JOptionPane.showMessageDialog(null, name + " the " + type + " has been created!");
+                    System.out.println("All pets: " + getAllPets());
+                }
+                else if (petCount == MAX_PETS) {
+                    JOptionPane.showMessageDialog(null, "Maximum number of pets reached!");
+                }
+                else if (!uniqueName) {
+                    JOptionPane.showMessageDialog(null, "Pet with name \"" + name + "\" already exists!");
+                }
             }
-
-            // Enforce max pet limit
-            if (AnimalGUI.petDatabase.size() >= MAX_PETS) {
-                JOptionPane.showMessageDialog(this, "Maximum number of pets reached!");
-                return;
-            }
-
-            // Default attributes for simplicity
-            int health = 10, attention = 10, currentAge = 1, lifeExpectancy = 10;
-            Pet newPet = null;
-
-            // Create the appropriate type of pet
-            switch (type) {
-                case "Dog":
-                    newPet = new Dog("Unknown Breed", name, "Dog", attention, health, currentAge, lifeExpectancy, color, sex);
-                    break;
-                case "Cat":
-                    newPet = new Cat(name, "Cat", attention, health, currentAge, lifeExpectancy, color, sex, "Unknown Breed");
-                    break;
-                case "Bird":
-                    newPet = new Bird("Unknown Breed", name, "Bird", attention, health, currentAge, lifeExpectancy, color, sex);
-                    break;
-                case "Fish":
-                    newPet = new Fish(name, "Fish", attention, health, currentAge, lifeExpectancy, color, sex, "Unknown Breed");
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(this, "Invalid pet type.");
-                    return;
-            }
-
-            // Add to pet database and display
-            AnimalGUI.petDatabase.put(name, newPet);
-            petDisplayPanel.addPetPanel(createPetPanel(newPet, petDetailsPanel));
-
-            JOptionPane.showMessageDialog(this, name + " the " + type + " has been created!");
         });
-    }
-
-    // Helper method to create a panel for a pet
-    private JPanel createPetPanel(Pet pet, PetDetailsPanel petDetailsPanel) {
-        JPanel petPanel = new JPanel();
-        petPanel.setLayout(new BoxLayout(petPanel, BoxLayout.Y_AXIS));
-        petPanel.setPreferredSize(new Dimension(262, 242));
-        petPanel.setBackground(pet.getColor());
-
-        JLabel petNameLabel = new JLabel(pet.getName());
-        JLabel petImageLabel = new JLabel(getPetImage(pet.getType()));
-
-        if (pet.getColor().equals(Color.BLACK) || pet.getColor().equals(Color.BLUE)
-                || pet.getColor().equals(Brown) || pet.getColor().equals(Color.DARK_GRAY)) {
-            petNameLabel.setForeground(Color.WHITE);
-            petImageLabel.setForeground(Color.WHITE);
-        }
-
-        JButton detailsButton = new JButton("View Details");
-        detailsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        detailsButton.addActionListener(e -> petDetailsPanel.showPetDetails(pet));
-
-        petNameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        petImageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        petNameLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        petImageLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
-        petPanel.add(petNameLabel);
-        petPanel.add(petImageLabel);
-        petPanel.add(detailsButton);
-
-        return petPanel;
-    }
-
-    // Helper method to get pet image as HTML
-    private String getPetImage(String type) {
-        String img = switch (type) {
-            case "Dog" -> dogimg;
-            case "Cat" -> catimg;
-            case "Bird" -> birdimg;
-            case "Fish" -> fishimg;
-            default -> "";
-        };
-        return "<html><div style='text-align: center; font-family: monospace;'>" +
-                img.replace(" ", "&nbsp;").replace("<", "&lt;").replace("\n", "<br>") +
-                "</div></html>";
     }
 
     // Helper method to convert color names to Color objects
@@ -184,7 +279,7 @@ public class PetManagementPanel extends JPanel {
             case "Dark Gray":
                 return Color.darkGray;
             case "Black":
-               return Color.black;
+                return Color.black;
             case "Brown":
                 return Brown;
             case "Beige":
@@ -192,5 +287,14 @@ public class PetManagementPanel extends JPanel {
             default:
                 return Color.WHITE;
         }
+    }
+
+    // Method to return a string representation of all pets
+    private String getAllPets() {
+        StringBuilder petList = new StringBuilder();
+        for (int i = 0; i < petCount; i++) {
+            petList.append(pets[i].toString()).append("\n");
+        }
+        return petList.toString();
     }
 }
